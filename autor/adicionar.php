5bdbfb36a0ee5b9960921c203df6cc647c44a0b8
Conceição Neves Silva <neves.conce@gmail.com>
2023-02-05 17:12:11 -0400

    up

--- autor/adicionar.php
+++ autor/adicionar.php
@@ -3,48 +3,30 @@
 include_once 'includes/header.php';
 include_once 'includes/message.php';
 ?> 
-
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <link rel="stylesheet" href="assets/css/cad-padrao.css" />
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width" />
-    <title>Cadastrar Autor</title>
-  </head>
-  <body>
-    <header>
-      <a href="cadastro.html">Voltar</a>
-      <ul>
-        <li><a href="login.html">Sair</a></li>
-      </ul>
-    </header>
-    <div class="card-cadastro">
-      <form class="card" action="php_action/create.php" method="POST">
-        <h1>CADASTRAR AUTOR</h1>
-        <div class="input-text">
-          <label for="nome">Nome:</label>
-          <input type="text" name="nomeAutor" id="nomeAutor">
-        </div>
-        <div class="input-text">
-          <label for="nome-art">Sobrenome:</label>
-          <input type="text" name="sobrenomeAutor" id="sobrenomeAutor">
-        </div>
-        <div class="input-text">
-          <label for="data-nascimento">Data Nascimento:</label>
-          <input type="date" name="dataNascimento" id="dataNascimento" />
-        </div>
-        <div class="input-text">
-          <label for="data-nascimento">Data Falecimento:</label>
-          <input type="date" name="dataMorte" id="dataMorte" />
-        </div>
-        <input type="submit" value="CADASTRAR" />
-      </form>
-    </div>
-  </body>
-</html>
-
+<body style="background-color: #8b7293">
+<body background="../atestes.png"> 
+<br><br><br>
+<div class="container">
+<div class="card-panel purple lighten-5">
+    <div class ="col s12 m6 push-m3">
+    <a href="index.php" class=""><i class="material-icons">arrow_back_ios</i></a>
+        <h3 class ="light">Novo Autor</h3>
+        <form action="php_action/create.php" method="POST">
+            <div class="input-field col s12">
+                <i class="material-icons prefix">account_circle</i>
+                <input id="icon_prefix" type="text" class="validate" type="text" name="nomeAutor" id="nomeAutor">
+                <label for="icon_prefix">Nome</label>
+            </div>
+            <div class="input-field col s12">
+                <i class="material-icons prefix">account_circle</i>
+                <input type="text" class="validate" type="text" name="sobrenomeAutor" id="sobrenomeAutor">
+                <label for="sobrenome">Sobrenome</label>
+            </div>
+            <div class="input-field col s12">
+                <i class="material-icons prefix">calendar_month</i>
+                <input id="icon_prefix" class="validate" type="date" name="dataNascimento" id="dataNascimento">
+                <label for="dataNascimento">Data de Nascimento</label>
+            </div>
             <div class="input-field col s12">
                 <i class="material-icons prefix">calendar_month</i>
                 <input id="icon_prefix" class="validate" type="date" name="dataMorte" id="dataMorte">
