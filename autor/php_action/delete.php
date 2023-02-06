2e2bd87833f751578e63fb5eeade567779c1fcfc
Conceição Neves Silva <neves.conce@gmail.com>
2023-02-06 18:48:50 -0400

    Update delete.php

--- autor/php_action/delete.php
+++ autor/php_action/delete.php
@@ -6,7 +6,7 @@ require_once 'bdconnect.php';
 if(isset($_POST['btn-deletar'])):
     $idAutor = mysqli_escape_string($connect, $_POST['idAutor']);
 
-    $sql = "DELETE FROM fornecedor WHERE idFornecedor ='$idFornecedor'";
+    $sql = "DELETE FROM autor WHERE idAutor ='$idAutor'";
     if(mysqli_query($connect, $sql)):
         $_SESSION['mensagem'] = "Deletado com sucesso!";
         header('Location: ../index.php');
