6b5e28e9fcf3b5a4753012363f3eb328fa933967
Maryse Pires <marysepires12@gmail.com>
2023-02-12 17:11:59 -0300

    .

--- venda/includes/header2.php
+++ venda/includes/header2.php
@@ -3,8 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Nova Venda</title>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
       <!--Import materialize.css-->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
