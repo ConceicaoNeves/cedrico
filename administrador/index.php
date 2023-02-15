47e24936fc6d4b41a56069061b9e5b088ed19280
Carolina Ambrósio <mcarolinafambrosio13@gmail.com>
2023-02-15 10:49:15 -0400

    .

--- administrador/index.php
+++ administrador/index.php
@@ -23,40 +23,41 @@ if($_SESSION["log"] == false) print "<script>location.href='index.php';</script>
         </ul>
         </header>
         <div class="card-cadastro">
-        <form class="card-list">
-            <h4>Administrador</h4>
-            <table>
-                <thread>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Sobrenome</th>
-                    </tr>
-                </thread>
-                <tbody>
-                    <?php
-                    $sql = "SELECT * FROM administrador";
-                    $resultado = mysqli_query($connect, $sql);
-                    if(mysqli_num_rows($resultado)>0):   
-                    while($dados = mysqli_fetch_array($resultado)):
-                    ?>
-                    <tr>
-                        <td><?php echo $dados['nomeAdmin']; ?></td>
-                        <td><?php echo $dados['sobrenomeAdmin']; ?></td>
-                        <td><a href="editar.php?idAdmin=<?php echo $dados['idAdmin']; ?>" class ="btn-floating light-green"><i class ="material-icons">edit</i></a></td> 
-                    </tr>
-                    <?php 
-                    endwhile;
-                    else: ?>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <?php 
-                    endif;
-                    ?>
-                </tbody>
-            </table> 
-        </form>       
+            <form class="card-list">
+                <h4>Administrador</h4>
+                <table>
+                    <thread>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Sobrenome</th>
+                        </tr>
+                    </thread>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM administrador";
+                        $resultado = mysqli_query($connect, $sql);
+                        if(mysqli_num_rows($resultado)>0):   
+                        while($dados = mysqli_fetch_array($resultado)):
+                        ?>
+                        <tr>
+                            <td><?php echo $dados['nomeAdmin']; ?></td>
+                            <td><?php echo $dados['sobrenomeAdmin']; ?></td>
+                            <td><a href="editar.php?idAdmin=<?php echo $dados['idAdmin']; ?>" class ="btn-floating light-green"><i class ="material-icons">edit</i></a></td> 
+                        </tr>
+                        <?php 
+                        endwhile;
+                        else: ?>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <?php 
+                        endif;
+                        ?>
+                    </tbody>
+                </table> 
+            </form>  
+        </div>     
     </body>
 </html>
 <?php
